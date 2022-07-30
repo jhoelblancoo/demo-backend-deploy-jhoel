e33378e6251027a41fa3861067412b3f27c1d1fc
jhoelblancoo <jhoel.blanco@correo.unimet.edu.ve>
2022-07-29 21:26:21 -0400

    prueba con otra ruta de api

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
     res.status(200).send("Hello server is running").end();
 });
 
+app.get("/hola", (req, res) => {
+    res.status(200).send("pruebaaaaa").end();
+});
+
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
